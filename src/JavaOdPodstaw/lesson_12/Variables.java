63b7bed3b9465ffaa0af2a6ca694ad548aaef42a
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-01-06 15:22:40 +0000

    Variable lesson finish

---
+++ src/JavaOdPodstaw/lesson_12/Variables.java
@@ -0,0 +1,84 @@
+package JavaOdPodstaw.lesson12;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte bmax = Byte.MAX_VALUE;
+        byte bmin = Byte.MIN_VALUE;
+
+        System.out.println("byte max =" + bmax);
+        System.out.println("byte min =" + bmin);
+
+        System.out.println("-----------------------------------------------------------------");
+
+        short smax = Short.MAX_VALUE;
+        short smin = Short.MIN_VALUE;
+
+        System.out.println("short max =" + smax);
+        System.out.println("short min =" + smin);
+
+        System.out.println("-----------------------------------------------------------------");
+
+        int imax = Integer.MAX_VALUE;
+        int imin = Integer.MIN_VALUE;
+
+        System.out.println("integer max =" + imax);
+        System.out.println("integer min =" + imin);
+
+        System.out.println("-----------------------------------------------------------------");
+
+        long lmax = Long.MAX_VALUE;
+        long lmin = Long.MIN_VALUE;
+
+        System.out.println("long max =" + lmax);
+        System.out.println("long min =" + lmin);
+
+
+        System.out.println("-----------------------------------------------------------------");
+
+        float fmax = Float.MAX_VALUE;
+        float fmin = Float.MIN_VALUE;
+
+        System.out.println("float max =" + fmax);
+        System.out.println("float min =" + fmin);
+
+
+        System.out.println("-----------------------------------------------------------------");
+
+        double dmax = Double.MAX_VALUE;
+        double dmin = Double.MIN_VALUE;
+
+        System.out.println("double max =" + dmax);
+        System.out.println("double min =" + dmin);
+
+        System.out.println("-----------------------------------------------------------------");
+
+        char chr = 'a';
+        String str = "Hello string";
+
+        System.out.println("char = " + chr);
+        System.out.println("str = " + str);
+
+        System.out.println("-----------------------------------------------------------------");
+
+        boolean btrue = true;
+        boolean bfalse = false;
+
+        System.out.println("Boolen true = " + btrue);
+        System.out.println("Boolen false = " + bfalse);
+
+
+        System.out.println("-----------------------------------------------------------------");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
