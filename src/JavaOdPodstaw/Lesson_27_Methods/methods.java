8d56d61c6d4bb86a3dce11c386807e2fbc04bbeb
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-03-15 16:52:38 +0100

    Lesson 27 finish

---
+++ src/JavaOdPodstaw/Lesson_27_Methods/methods.java
@@ -0,0 +1,15 @@
+package JavaOdPodstaw.methods;
+
+public class methods {
+
+    private static void sayHi(String name) {
+        System.out.println("Hello "+name);
+    }
+
+    public static void main(String[] args) {
+        sayHi("Piotr");
+
+    }
+
+
+}
