4a5ce6515170a6c81491dccc8ddfb8ad1ab6707b
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-03-15 19:20:26 +0100

    Return finish

--- src/JavaOdPodstaw/Lesson_27_Methods/methods.java
+++ src/JavaOdPodstaw/Lesson_27_Methods/methods.java
@@ -1,15 +1,28 @@
-package JavaOdPodstaw.methods;
+package JavaOdPodstaw.Lesson_27_Methods;
 
 public class methods {
 
-    private static void sayHi(String name) {
-        System.out.println("Hello "+name);
-    }
+//    public static void main(String[] args) {
+//        sayHi1("Tomek", "Marek");
+//    }
 
     public static void main(String[] args) {
-        sayHi("Piotr");
+        sayHi2();
+        sayHi2("Tomek", "Marek", "Wojtek");
+
 
     }
 
 
+    private static void sayHi1(String name, String name2) {
+        System.out.println("Hello " + name + " ," + name2);
+    }
+
+    private static void sayHi2(String... names) {
+        String result = "";
+        for (String s : names) {
+            result = result + s + ", ";
+        }
+        System.out.println("Hi " + result);
+    }
 }
