da9711e302a801a926a9ee1201bc056a93940e03
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-03-16 13:57:21 +0100

    Lesson 32 Getter and Setter finish

--- src/JavaOdPodstaw/Lesson_32_GetterAndSetter/Address.java
+++ src/JavaOdPodstaw/Lesson_32_GetterAndSetter/Address.java
@@ -1,4 +1,33 @@
 package JavaOdPodstaw.Lesson_32_GetterAndSetter;
 
 public class Address {
+
+
+    private String StreetNameAndHouseNumber;
+    private String CityName;
+    private String postCode;
+
+    public String getStreetNameAndHouseNumber() {
+        return StreetNameAndHouseNumber;
+    }
+
+    public void setStreetNameAndHouseNumber(String streetNameAndHouseNumber) {
+        StreetNameAndHouseNumber = streetNameAndHouseNumber;
+    }
+
+    public String getCityName() {
+        return CityName;
+    }
+
+    public void setCityName(String cityName) {
+        CityName = cityName;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
 }
--- src/JavaOdPodstaw/Lesson_32_GetterAndSetter/GetterAndSetter.java
+++ src/JavaOdPodstaw/Lesson_32_GetterAndSetter/GetterAndSetter.java
@@ -1,4 +1,44 @@
 package JavaOdPodstaw.Lesson_32_GetterAndSetter;
 
 public class GetterAndSetter {
+
+    public static void main(String[] args) {
+
+
+        Client clientMichal = new Client();
+        clientMichal.setFirstName("Michal");
+        clientMichal.setLastName("Forbs");
+
+
+        Address michalAddress = new Address();
+        michalAddress.setStreetNameAndHouseNumber("100 Victoria St");
+        michalAddress.setCityName("London");
+        michalAddress.setPostCode("LO16 4PA");
+
+        clientMichal.setAddress(michalAddress);
+
+
+        System.out.print(clientMichal.getFirstName());
+        System.out.print(" ");
+        System.out.println(clientMichal.getLastName());
+        System.out.println(Client.COMPANY_NAME);
+        System.out.println("----------------");
+        System.out.println(clientMichal.getAddress().getStreetNameAndHouseNumber());
+        System.out.println(clientMichal.getAddress().getCityName());
+        System.out.println(clientMichal.getAddress().getPostCode());
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
 }
