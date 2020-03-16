63af1dab1ba4bc32a45a32bcea79846ae94e7bf0
PiotrKoniorczyk <piotrek.koniorczyk@gmail.com>
2020-03-16 16:35:28 +0100

    Lesson 34 Methods Finish

--- src/JavaOdPodstaw/Lesson_34_Methods/Methods.java
+++ src/JavaOdPodstaw/Lesson_34_Methods/Methods.java
@@ -1,4 +1,18 @@
 package JavaOdPodstaw.Lesson_34_Methods;
 
 public class Methods {
+    public static void main(String[] args) {
+
+
+        Client_Methods clientMichal = new Client_Methods();
+        clientMichal.setFirstName("Michal");
+        clientMichal.setLastName("Forbs");
+
+        clientMichal.introduceYourSelf();
+
+
+
+
+    }
+
 }
